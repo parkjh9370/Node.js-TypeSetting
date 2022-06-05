bac5a3346ab0210e82d18fa6d4ec905da81339f5
Jaehyeon <parkjh9370@naver.com>
2022-06-05 09:41:47 +0900

    node to type

--- allowJS-Typescript/dist/app.js
+++ allowJS-Typescript/dist/app.js
@@ -15,11 +15,11 @@ app.use(morgan("tiny"));
 app.use("/tweets", tweetsRouter);
 app.use("/auth", authRouter);
 app.use((req, res, next) => {
-    res.sendStatus(404);
+  res.sendStatus(404);
 });
 app.use((error, req, res, next) => {
-    console.error(error);
-    res.sendStatus(500);
+  console.error(error);
+  res.sendStatus(500);
 });
 const server = app.listen(config.host.port);
 initSocket(server);
